d8f6e3c0044828ab4527f88df99c63a2b5868ebd
Ganesh <princeganna525@gmail.com>
2026-07-28 21:40:19 +0530

    Add fallback web/build.sh to resolve Vercel Root Directory 127 error

--- web/build.sh
+++ web/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# If executed inside web directory, move to repo root
+if [ -f "../pubspec.yaml" ]; then
+  cd ..
+fi
+
 # Download Flutter SDK into build environment if not present
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
